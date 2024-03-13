c0eba4064e86a3f36f91d6f698c82dbd1ac5752e
Jasczlv <ianis.cozlov@gmail.com>
2024-03-13 20:40:03 +0100

    ADD js

--- js/app.js
+++ js/app.js
@@ -2,10 +2,29 @@ const play = document.querySelector(".start");
 
 const row = document.querySelector(".row.f-wrap");
 
-// console.log(row);
+const facile = document.querySelector("option[value=facile]");
+const media = document.querySelector("option[value=media]");
+const difficile = document.querySelector("option[value=difficile]");
 
+let lato = 0;
+
+facile.addEventListener("click", function () {
+  nameFacile = "col-facile";
+  lato = 10;
+});
+media.addEventListener("click", function () {
+  nameFacile = "col-media";
+  lato = 9;
+});
+difficile.addEventListener("click", function () {
+  nameFacile = "col-difficile";
+  lato = 7;
+});
+
+// console.log(row);
+let nameFacile = "";
 play.addEventListener("click", function () {
-  let lato = 10;
+  console.log(lato);
   let quadrato = lato ** 2;
   for (i = 0; i < quadrato; i++) {
     createGrid();
@@ -15,7 +34,8 @@ play.addEventListener("click", function () {
 function createGrid() {
   const cell = document.createElement("div");
   row.append(cell);
-  cell.className = "col-facile";
+
+  cell.className = nameFacile;
   cell.innerHTML = i + 1;
   cell.addEventListener("click", function () {
     cell.classList.toggle("col-clicked");
