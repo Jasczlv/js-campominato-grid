5cb794800d5da2d33e37f51e46b8b787cb6929c3
Jasczlv <ianis.cozlov@gmail.com>
2024-03-13 19:52:05 +0100

    ADD html css js

--- js/app.js
+++ js/app.js
@@ -5,20 +5,19 @@ const row = document.querySelector(".row.f-wrap");
 // console.log(row);
 
 play.addEventListener("click", function () {
-  const facile = document.querySelector('input[value = "facile"]');
-  console.log("asd");
   let lato = 10;
   let quadrato = lato ** 2;
   for (i = 0; i < quadrato; i++) {
-    const cell = document.createElement("div");
-    row.append(cell);
-    cell.className = "col-facile";
-    cell.innerHTML = i + 1;
-    cell.addEventListener("click", function () {
-      cell.classList.toggle("col-clicked");
-    });
+    createGrid();
   }
 });
 
-const media = document.querySelector('input[value = "media"]');
-const difficile = document.querySelector('input[value = "difficile"]');
+function createGrid() {
+  const cell = document.createElement("div");
+  row.append(cell);
+  cell.className = "col-facile";
+  cell.innerHTML = i + 1;
+  cell.addEventListener("click", function () {
+    cell.classList.toggle("col-clicked");
+  });
+}
